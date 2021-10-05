68c37f1b6a98f029f2bcf48f45a440fbfbd0af5c
Tjerk Nagel <tjerk.nagel90@gmail.com>
2021-10-05 09:15:45 +0200

    start.spring.io generated

---
+++ src/main/java/nl/miwgroningen/se6/gardengnomes/Igadi/IgadiApplication.java
@@ -0,0 +1,13 @@
+package nl.miwgroningen.se6.gardengnomes.Igadi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgadiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgadiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/miwgroningen/se6/gardengnomes/Igadi/IgadiApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.miwgroningen.se6.gardengnomes.Igadi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgadiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
