3183aca28095201f2a86581efcf31cc1b8d5ee5d
Annemarleen <amholbosma@gmail.com>
2021-10-07 15:46:54 +0200

    Added seperate file for sql files

--- src/main/resources/insertDBtestcases.sql
+++ src/main/resources/insertDBtestcases.sql
@@ -1,15 +1,3 @@
 INSERT INTO garden (gardenId, gardenName) VALUES (1, "Eden");
 INSERT INTO garden (gardenId, gardenName) VALUES (2, "Noorderplantsoen");
 INSERT INTO garden (gardenId, gardenName) VALUES (3, "Prinsjestuin");
-
-INSERT INTO patch (gardenId, patchId) VALUES (1, 1);
-INSERT INTO patch (gardenId, patchId) VALUES (1, 2);
-INSERT INTO patch (gardenId, patchId) VALUES (1, 3);
-
-INSERT INTO patch (gardenId, patchId) VALUES (2, 1);
-INSERT INTO patch (gardenId, patchId) VALUES (2, 2);
-INSERT INTO patch (gardenId, patchId) VALUES (2, 3);
-
-INSERT INTO patch (gardenId, patchId) VALUES (3, 1);
-INSERT INTO patch (gardenId, patchId) VALUES (3, 2);
-INSERT INTO patch (gardenId, patchId) VALUES (3, 3);
