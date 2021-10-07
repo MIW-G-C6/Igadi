bf684e1ba86e0466bd29ae3dd5a290ace7ba767c
Annemarleen <amholbosma@gmail.com>
2021-10-07 16:23:56 +0200

    Changed first letter of table names to uppercase.

--- src/main/resources/insertDBtestcases.sql
+++ src/main/resources/insertDBtestcases.sql
@@ -1,7 +1,7 @@
-INSERT INTO garden (gardenId, gardenName) VALUES (1, "Eden");
-INSERT INTO garden (gardenId, gardenName) VALUES (2, "Noorderplantsoen");
-INSERT INTO garden (gardenId, gardenName) VALUES (3, "Prinsjestuin");
+INSERT INTO Garden (gardenId, gardenName) VALUES (1, "Eden");
+INSERT INTO Garden (gardenId, gardenName) VALUES (2, "Noorderplantsoen");
+INSERT INTO Garden (gardenId, gardenName) VALUES (3, "Prinsjestuin");
 
-INSERT INTO patch (patchId, garden_gardenId) VALUES (1, 1);
-INSERT INTO patch (patchId, garden_gardenId) VALUES (2, 2);
-INSERT INTO patch (patchId, garden_gardenId) VALUES (3, 3);
+INSERT INTO Patch (patchId, garden_gardenId) VALUES (1, 1);
+INSERT INTO Patch (patchId, garden_gardenId) VALUES (2, 2);
+INSERT INTO Patch (patchId, garden_gardenId) VALUES (3, 3);
