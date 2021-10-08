114d4ae89dca1914e41de842c3f380bca69c27ab
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2021-10-08 13:18:25 +0200

    added task to taskcontainer

--- src/main/resources/sqlFiles/insertDBtestcases.sql
+++ src/main/resources/sqlFiles/insertDBtestcases.sql
@@ -2,9 +2,15 @@ INSERT INTO Garden (gardenId, gardenName) VALUES (1, "Eden");
 INSERT INTO Garden (gardenId, gardenName) VALUES (2, "Noorderplantsoen");
 INSERT INTO Garden (gardenId, gardenName) VALUES (3, "Prinsjestuin");
 
+INSERT INTO Garden (gardenId, gardenName) VALUES (4, "Holleboom");
+INSERT INTO Garden (gardenId, gardenName) VALUES (5, "Aardplaat");
+INSERT INTO Garden (gardenId, gardenName) VALUES (6, "Schuiftuin");
+INSERT INTO Garden (gardenId, gardenName) VALUES (7, "Stadspark");
+INSERT INTO Garden (gardenId, gardenName) VALUES (8, "Kostverloren");
+
 INSERT INTO Patch (patchId, garden_gardenId) VALUES (1, 1);
 INSERT INTO Patch (patchId, garden_gardenId) VALUES (2, 2);
 INSERT INTO Patch (patchId, garden_gardenId) VALUES (3, 3);
 
-INSERT INTO Task (taskId, taskName, taskDescription, isDone) VALUES (1, "Watering the plants", "Watering the plants", false);
-INSERT INTO Task (taskId, taskName, taskDescription, isDone) VALUES (2, "Feeding the garden", "Feeding the garden", true);
+INSERT INTO Task (taskId, taskName, taskDescription, isDone, garden_gardenId, patch_patchId) VALUES (1, "Watering the plants", "Watering the plants", false, 1, 1);
+INSERT INTO Task (taskId, taskName, taskDescription, isDone, garden_gardenId, patch_patchId) VALUES (2, "Feeding the garden", "Feeding the garden", true, 2, 2);
