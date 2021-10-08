29a4543b4f494236d130faf1aed9aa71ec143986
Annemarleen <amholbosma@gmail.com>
2021-10-08 11:00:54 +0200

    service map

---
+++ src/main/java/nl/miwgroningen/se6/gardengnomes/Igadi/services/TaskServices.java
@@ -0,0 +1,12 @@
+package nl.miwgroningen.se6.gardengnomes.Igadi.services;
+
+/**
+ * @author Annemarleen Bosma <makeItWokr2021@annemarleenbosma.nl>
+ * Dit is wat het programma doet:
+ */
+
+public class TaskServices {
+
+
+
+}
