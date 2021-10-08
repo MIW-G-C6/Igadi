0c067fcfe9f2c466737be6bffa9c8872e6b5743a
Annemarleen <amholbosma@gmail.com>
2021-10-08 11:50:12 +0200

    Slice of Task done. Two tasks in database as example

--- src/main/resources/insertDBtestcases.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO Garden (gardenId, gardenName) VALUES (1, "Eden");
-INSERT INTO Garden (gardenId, gardenName) VALUES (2, "Noorderplantsoen");
-INSERT INTO Garden (gardenId, gardenName) VALUES (3, "Prinsjestuin");
-
-INSERT INTO Patch (patchId, garden_gardenId) VALUES (1, 1);
-INSERT INTO Patch (patchId, garden_gardenId) VALUES (2, 2);
-INSERT INTO Patch (patchId, garden_gardenId) VALUES (3, 3);
