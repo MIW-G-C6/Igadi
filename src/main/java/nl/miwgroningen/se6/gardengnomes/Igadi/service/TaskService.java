7d319c09092540d2c1da1c610fdf71233503d0b7
Annemarleen <amholbosma@gmail.com>
2021-10-11 14:48:33 +0200

    Lege TaskService

--- src/main/java/nl/miwgroningen/se6/gardengnomes/Igadi/service/TaskService.java
+++ src/main/java/nl/miwgroningen/se6/gardengnomes/Igadi/service/TaskService.java
@@ -1,12 +1,14 @@
 package nl.miwgroningen.se6.gardengnomes.Igadi.service;
 
+import nl.miwgroningen.se6.gardengnomes.Igadi.model.Task;
+
+import java.util.List;
+
 /**
  * @author Annemarleen Bosma <makeItWokr2021@annemarleenbosma.nl>
  * Dit is wat het programma doet:
  */
 
 public class TaskService {
-
-
 
 }
