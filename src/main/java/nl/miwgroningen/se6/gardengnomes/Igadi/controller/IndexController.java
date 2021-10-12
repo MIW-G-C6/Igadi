b84593393c1f106df736b07830918afac56f0f62
Annemarleen <amholbosma@gmail.com>
2021-10-12 16:54:40 +0200

    finist index.html

---
+++ src/main/java/nl/miwgroningen/se6/gardengnomes/Igadi/controller/IndexController.java
@@ -0,0 +1,7 @@
+package nl.miwgroningen.se6.gardengnomes.Igadi.controller;/**
+* @author Annemarleen Bosma <makeItWokr2021@annemarleenbosma.nl>
+* Dit is wat het programma doet:
+*
+*/
+public class IndexController {
+}
