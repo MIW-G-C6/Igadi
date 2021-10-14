a8f83a0664c67116e8d0f123a8f50f156713c673
Tjerk Nagel <tjerk.nagel90@gmail.com>
2021-10-14 12:02:03 +0200

    createOrUpdateGarden error handling and message in view

--- src/main/java/nl/miwgroningen/se6/gardengnomes/Igadi/controller/GardenController.java
+++ src/main/java/nl/miwgroningen/se6/gardengnomes/Igadi/controller/GardenController.java
@@ -3,12 +3,15 @@ package nl.miwgroningen.se6.gardengnomes.Igadi.controller;
 import nl.miwgroningen.se6.gardengnomes.Igadi.model.Garden;
 import nl.miwgroningen.se6.gardengnomes.Igadi.repository.GardenRepository;
 import nl.miwgroningen.se6.gardengnomes.Igadi.service.GardenService;
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
  * @author Tjerk Nagel
@@ -29,19 +32,37 @@ public class GardenController {
         model.addAttribute("allGardens", gardenService.getAllGardens());
         return "gardens";
     }
-    // TODO make a button to create a new garden
 
     @GetMapping("/gardens/new")
-    protected String showGardenForm(Model model) {
+    protected String showGardenForm(Model model, String errorMessage, String errorMessageColor) {
         model.addAttribute("garden", new Garden());
+        if (errorMessage != null) {
+            model.addAttribute("errorMessage", errorMessage);
+            model.addAttribute("errorMessageColor", errorMessageColor);
+        }
         return "gardenForm";
     }
-    // TODO sort by id before returning
+    // TODO sort by id before returning?
 
     @PostMapping("gardens/new")
-    protected String createOrUpdateGarden(@ModelAttribute("garden") Garden garden, BindingResult result) {
+    protected String createOrUpdateGarden(@ModelAttribute("garden") Garden garden, BindingResult result,
+                                          RedirectAttributes redirectAttributes) {
         if (!result.hasErrors()) {
-            gardenService.saveGarden(garden);
+            try {
+                gardenService.saveGarden(garden);
+            } catch (DataIntegrityViolationException ex) {
+                String errorMessage = "That name already exists.";
+                redirectAttributes.addAttribute("errorMessage", errorMessage);
+                redirectAttributes.addAttribute("errorMessageColor", "redErrorMessage");
+                System.out.println(ex);
+                return "redirect:/gardens/new";
+            } catch (Exception ex) {
+                String errorMessage = "Something went wrong.";
+                redirectAttributes.addAttribute("errorMessage", errorMessage);
+                redirectAttributes.addAttribute("errorMessageColor", "redErrorMessage");
+                System.out.println(ex);
+                return "redirect:/gardens/new";
+            }
         }
         return "redirect:/gardens";
     }
