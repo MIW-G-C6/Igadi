77af117cedee1334ae8bccfc7225eb92356d9461
Ruerd de Jong <Ruerd_here@hotmail.com>
2021-11-04 16:19:32 +0100

    Adjusted Html pages, first version gardenoverview page

--- src/test/java/nl/miwgroningen/se6/gardengnomes/Igadi/IgadiApplicationTests.java
+++ src/test/java/nl/miwgroningen/se6/gardengnomes/Igadi/IgadiApplicationTests.java
@@ -3,11 +3,11 @@ package nl.miwgroningen.se6.gardengnomes.Igadi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class IgadiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class IgadiApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
