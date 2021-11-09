5210d781ea4e3ff7cd5ab6fd4b9e37d9eb4d1262
Tjerk Nagel <tjerk.nagel90@gmail.com>
2021-11-09 12:04:29 +0100

    Manager authorization for patchTask create get+post

--- src/main/java/nl/miwgroningen/se6/gardengnomes/Igadi/service/PatchTaskService.java
+++ src/main/java/nl/miwgroningen/se6/gardengnomes/Igadi/service/PatchTaskService.java
@@ -1,6 +1,7 @@
 package nl.miwgroningen.se6.gardengnomes.Igadi.service;
 
 import nl.miwgroningen.se6.gardengnomes.Igadi.dto.PatchTaskDTO;
+import nl.miwgroningen.se6.gardengnomes.Igadi.helpers.AuthorizationHelper;
 import nl.miwgroningen.se6.gardengnomes.Igadi.model.PatchTask;
 import nl.miwgroningen.se6.gardengnomes.Igadi.repository.PatchTaskRepository;
 import org.springframework.stereotype.Service;
@@ -16,14 +17,25 @@ public class PatchTaskService {
 
     private PatchTaskRepository patchTaskRepository;
     private PatchService patchService;
+    private AuthorizationHelper authorizationHelper;
 
-    public PatchTaskService(PatchTaskRepository patchTaskRepository, PatchService patchService) {
+    public PatchTaskService(PatchTaskRepository patchTaskRepository, PatchService patchService,
+                            AuthorizationHelper authorizationHelper) {
         this.patchTaskRepository = patchTaskRepository;
         this.patchService = patchService;
+        this.authorizationHelper = authorizationHelper;
     }
 
     public void savePatchTask(PatchTask patchTask) {
         patchTaskRepository.save(patchTask);
+    }
+
+    public void userSavePatchTask(PatchTask patchTask, int userId, int gardenId) {
+        if (authorizationHelper.isUserGardenManager(userId, gardenId)) {
+            savePatchTask(patchTask);
+        } else {
+            throw new SecurityException("You are not allowed to save this patch task.");
+        }
     }
 
     public List<PatchTaskDTO> getAllPatchTasks() {
