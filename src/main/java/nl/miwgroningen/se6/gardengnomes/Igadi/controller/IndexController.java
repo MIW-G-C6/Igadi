b64ec32ab276eb1311cbb0b860647cf693ee32fb
Annemarleen <amholbosma@gmail.com>
2021-11-10 11:18:38 +0100

    Created landingprofilepage
    Created profile page, using index.html.
    It shows the user: their username, their gardens in which they are enrolled and their roles in that particular garden.

--- src/main/java/nl/miwgroningen/se6/gardengnomes/Igadi/controller/IndexController.java
+++ src/main/java/nl/miwgroningen/se6/gardengnomes/Igadi/controller/IndexController.java
@@ -1,10 +1,17 @@
 package nl.miwgroningen.se6.gardengnomes.Igadi.controller;
 
+import nl.miwgroningen.se6.gardengnomes.Igadi.model.Garden;
+import nl.miwgroningen.se6.gardengnomes.Igadi.model.GardenUser;
 import nl.miwgroningen.se6.gardengnomes.Igadi.model.User;
+import nl.miwgroningen.se6.gardengnomes.Igadi.service.GardenService;
+import nl.miwgroningen.se6.gardengnomes.Igadi.service.GardenUserService;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Annemarleen Bosma <makeItWork2021@annemarleenbosma.nl>
@@ -15,10 +22,37 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
+    private GardenUserService gardenUserService;
+    private GardenService gardenService;
+
+    public IndexController(GardenUserService gardenUserService, GardenService gardenService) {
+        this.gardenUserService = gardenUserService;
+        this.gardenService = gardenService;
+    }
+
     @GetMapping({"/", "/index"})
     protected String showIndexPage(Model model, @AuthenticationPrincipal User user) {
         if(user != null) {
-            model.addAttribute(user);
+            user.setGardenUsers(gardenUserService.findAllGardenUsersByUserId(user.getUserId()));
+
+            ArrayList<Garden> gardens = new ArrayList<>();
+            for (GardenUser gardenUser : user.getGardenUsers()) {
+                Garden newGarden = gardenService.getGardenById(gardenUser.getGarden().getGardenId());
+                gardens.add(newGarden);
+            }
+            ArrayList<String> roles = new ArrayList<>();
+            for (Garden garden : gardens) {
+                int gardenId1 = garden.getGardenId();
+                for (GardenUser gardenUser : user.getGardenUsers()) {
+                    int gardenId2 = gardenUser.getGarden().getGardenId();
+                    if (gardenId1 == gardenId2) {
+                        roles.add(gardenUser.getRole());
+                    }
+                }
+            }
+            model.addAttribute("user", user);
+            model.addAttribute("gardens", gardens);
+            model.addAttribute("roles", roles);
         } else {
             user = new User();
             user.setUserName("admin");
@@ -31,5 +65,5 @@ public class IndexController {
     protected String showAboutPage(Model model) {
         return "about";
     }
-}
 
+}
