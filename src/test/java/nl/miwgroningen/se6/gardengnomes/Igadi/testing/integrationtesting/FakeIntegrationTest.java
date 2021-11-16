50668c94f2f04579afd20d0110c2b90ce4390c2c
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2021-11-16 11:11:17 +0100

    added fake tests

---
+++ src/test/java/nl/miwgroningen/se6/gardengnomes/Igadi/testing/integrationtesting/FakeIntegrationTest.java
@@ -0,0 +1,18 @@
+package nl.miwgroningen.se6.gardengnomes.Igadi.testing.integrationtesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author Lukas de Ruiter <lukas_kremlin@hotmail.com>
+ */
+
+@SpringBootTest
+public class FakeIntegrationTest {
+
+    @Test
+    void emptyIntegrationTest() {
+
+    }
+
+}
