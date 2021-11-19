467417cddbdf09f924f762839dbee848eae8c89e
Annemarleen <amholbosma@gmail.com>
2021-11-19 15:31:54 +0100

    redirect from create account
    When create account is successfull, you'll be redirected to loginpage

--- src/main/java/nl/miwgroningen/se6/gardengnomes/Igadi/controller/LoginController.java
+++ src/main/java/nl/miwgroningen/se6/gardengnomes/Igadi/controller/LoginController.java
@@ -4,6 +4,10 @@ import nl.miwgroningen.se6.gardengnomes.Igadi.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.ArrayList;
+
 
 /**
  * @author Annemarleen Bosma <makeItWork2021@annemarleenbosma.nl>
@@ -16,8 +20,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class LoginController {
 
     @GetMapping("/login")
-    protected String showLoginPage(Model model) {
+    protected String showLoginPage(Model model, @ModelAttribute("message") ArrayList<String> message) {
+
         model.addAttribute("user", new User());
+
+        if (!message.isEmpty()) {
+            model.addAttribute("message", message);
+        }
         return "login";
     }
 }
