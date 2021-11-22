71670e548f885f7e712cf3d3214b045ccfdc7ce1
Annemarleen <amholbosma@gmail.com>
2021-11-22 14:26:13 +0100

    IG-139 password eye
    The eye is now in the inputfield, so the layout stays consistent with the rest of the inputfields

--- src/main/resources/static/js/ToggleController.js
+++ src/main/resources/static/js/ToggleController.js
@@ -5,26 +5,32 @@
 
 $(document).ready(function() {
 
-    $("#icon-click").click(function() {
+    $("#icon-click").click(function(event) {
+        event.preventDefault();
 
-        let inputfieldPassword1 = $("#inputfieldPassword1");
-        let inputfieldRepeatPassword1 = $("#inputfieldRepeatPassword1");
+        togglePasswordEye();
+});
 
+function togglePasswordEye() {
+    let inputfieldPassword1 = $("#inputfieldPassword1");
+    let inputfieldRepeatPassword1 = $("#inputfieldRepeatPassword1");
+
+    // document.getElementById("iconPasswordEyeVisible").src="/images/icons/icons8-hide-24.png";
+    //
+    // if (inputfieldPassword1 == null){
+    //     document.getElementById("iconPasswordEyeVisible").src="/images/icons/icons8-hide-24.png";
+    //
+    // }
+
+    if (inputfieldPassword1.attr("type") === "password" || inputfieldRepeatPassword1 === "password"){
+        inputfieldPassword1.attr("type", "text");
+        inputfieldRepeatPassword1.attr("type", "text");
         document.getElementById("iconPasswordEyeVisible").src="/images/icons/icons8-hide-24.png";
+    } else {
+        inputfieldPassword1.attr("type", "password");
+        inputfieldRepeatPassword1.attr("type", "password");
+        document.getElementById("iconPasswordEyeVisible").src="/images/icons/icons8-eye-24.png";
+    }
+};
 
-        if (inputfieldPassword1 == null){
-            document.getElementById("iconPasswordEyeVisible").src="/images/icons/icons8-hide-24.png";
-
-        }
-
-        if (inputfieldPassword1.attr("type") === "password" || inputfieldRepeatPassword1 === "password"){
-            inputfieldPassword1.attr("type", "text");
-            inputfieldRepeatPassword1.attr("type", "text");
-            document.getElementById("iconPasswordEyeVisible").src="/images/icons/icons8-hide-24.png";
-        } else {
-            inputfieldPassword1.attr("type", "password");
-            inputfieldRepeatPassword1.attr("type", "password");
-            document.getElementById("iconPasswordEyeVisible").src="/images/icons/icons8-eye-24.png";
-        }
-    })
 });
